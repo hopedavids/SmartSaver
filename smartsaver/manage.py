cdc80f558c18121abd8adb356fa5bddbfa31e138
hopedavids <hopeolawole@gmail.com>
2023-10-17 16:25:54 +0000

    manage file

--- smartsaver/manage.py
+++ smartsaver/manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from .donate_app import app
+from .smartsaver_app import app
 from .instance import db
 
 
