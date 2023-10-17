64cbf5c1a0e093ff4d5371b03e5a59e926749cf9
hopedavids <hopeolawole@gmail.com>
2023-10-17 16:25:20 +0000

    api models

--- smartsaver/api_models.py
+++ smartsaver/api_models.py
@@ -80,10 +80,10 @@ contact_model = api.model(
     "Contact", {
         "contact_id": fields.Integer,
         "user": fields.Nested(user_model),
-        "full_name": fields.String,
+        "fullname": fields.String,
         "address": fields.String,
         "country": fields.String,
-        "about_me": fields.String
+        "description": fields.String
     }
 )
 
@@ -97,27 +97,25 @@ contact_update_model = api.model(
 )
 
 transfer_model = api.model(
-    "Donation", {
-        "donation_id": fields.Integer,
+    "Transfer_Money", {
+        "transfer_id": fields.Integer,
         "user": fields.Nested(user_model),
         "amount": fields.Float,
-        "tree_spieces": fields.String,
-        "number_of_trees": fields.String,
-        "region_to_plant": fields.String,
-        "description": fields.String,
+        "walletID": fields.String,
+        "recipientemail": fields.String,
+        "recipientfullname": fields.String,
         "get_certified": fields.Boolean,
         "timestamp": fields.DateTime(dt_format='iso8601')
     }
 )
 
 transfer_update_model = api.model(
-    "Donation", {
+    "Transfer_Money", {
         "userid": fields.Integer(required=True),
         "amount": fields.Integer(required=True),
-        "tree_spieces": fields.String(required=True),
-        "number_of_trees": fields.String(required=True),
-        "region_to_plant": fields.String(required=True),
-        "description": fields.String(required=True)
+        "walletID": fields.String(required=True),
+        "recipientemail": fields.String(required=True),
+        "recipientfullname": fields.String(required=True),
     }
 )
 
@@ -125,7 +123,7 @@ payment_model = api.model(
     "Payment", {
         "payment_id": fields.Integer,
         "wallet_id": fields.String,
-        "donation": fields.Nested(donation_model),
+        "transfer": fields.Nested(transfer_model),
         "amount": fields.Float,
         "timestamp": fields.DateTime(dt_format='iso8601')
     }
