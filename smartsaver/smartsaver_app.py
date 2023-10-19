c7fa2e3bf0812f2c490bb824b57315f611410672
hopedavids <hopeolawole@gmail.com>
2023-10-19 13:36:22 +0000

    modified the import

--- smartsaver/smartsaver_app.py
+++ smartsaver/smartsaver_app.py
@@ -12,7 +12,7 @@ from flask_session import Session
 from flask_restx import apidoc
 from instances import api, db, jwt, login_manager, csrf, mail
 from sqlalchemy.dialects.postgresql import psycopg2
-from resources import auth_ns, user_ns, wallet_ns, pay_ns, donation_ns, contact_ns
+from resources import auth_ns, user_ns, wallet_ns, pay_ns, transfer_ns, contact_ns
 from user_auth import user_auth as user_auth_blueprint
 from main import main as main_blueprint
 from api_auth import api_auth as api_auth_blueprint
@@ -87,7 +87,7 @@ def create_app():
     api.add_namespace(user_ns)
     api.add_namespace(wallet_ns)
     api.add_namespace(pay_ns)
-    api.add_namespace(donation_ns)
+    api.add_namespace(transfer_ns)
     api.add_namespace(contact_ns)
     # api.add_namespace(api_ns)
 
@@ -104,10 +104,6 @@ def create_app():
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(frontend_views_blueprint)
 
-    
-    
-
-    
     @api.documentation
     def customize_swagger_ui():
         return {
